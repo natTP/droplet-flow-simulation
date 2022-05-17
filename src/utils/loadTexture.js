5c1b94a8e956253a233fdfd3873949f92a699bd8
Nattamon Polvichai <joppy.pgh@gmail.com>
2022-05-17 23:20:45 +0700

    Fix resource not loaded

--- src/utils/loadTexture.js
+++ src/utils/loadTexture.js
@@ -1,7 +1,7 @@
 import { TextureLoader, RepeatWrapping, NearestFilter } from 'three'
 
 function loadPlaneTexture(planeSize) {
-    const texture = new TextureLoader().load('../assets/checker.png')
+    const texture = new TextureLoader().load('./assets/checker.png')
     texture.wrapS = RepeatWrapping
     texture.wrapT = RepeatWrapping
     texture.magFilter = NearestFilter
